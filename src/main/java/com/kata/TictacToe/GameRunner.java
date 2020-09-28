603206185f2b656df1acdbfb0871478aafccaf45
AMRITA <amritagolu678@gmail.com>
2020-09-29 00:38:09 +0530

    Initializing turn as static as X should play first

--- src/main/java/com/kata/TictacToe/GameRunner.java
+++ src/main/java/com/kata/TictacToe/GameRunner.java
@@ -1,17 +1,23 @@
 package com.kata.TictacToe;
 
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class GameRunner {
     static Game ticTacToeGame;
     static Logger log = Logger.getLogger(GameRunner.class.getName());
+    public static char turn= 'X';
 
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int positionNumber;
         log.info("Welcome to 2 Player Tic Tac Toe.");
         log.info("--------------------------------");
         ticTacToeGame = new Game();
         ticTacToeGame.initializeBoard();
         ticTacToeGame.printBoard();
+        char winner = 0;
+
     }
 
 }
