fd1bb33dbe24126c0bf40924bfb8828b899e864b
AMRITA <amritagolu678@gmail.com>
2020-09-28 21:29:27 +0530

    Returned player O for the position number 2

--- src/main/java/com/kata/TictacToe/Game.java
+++ src/main/java/com/kata/TictacToe/Game.java
@@ -1,7 +1,11 @@
 package com.kata.TictacToe;
 
 public class Game {
+
     public char getPlayer(int positionNumber) {
-        return 'X';
+        if (positionNumber == 1)
+            return 'X';
+        else
+            return 'O';
     }
 }
--- src/test/java/com/kata/TictacToe/GameTest.java
+++ src/test/java/com/kata/TictacToe/GameTest.java
@@ -1,14 +1,25 @@
 package com.kata.TictacToe;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GameTest {
 
+    private Game ticTacToeGame;
+    @Before
+    public void init() {
+        ticTacToeGame = new Game();
+    }
+
     @Test
     public void shouldReturnXOnFirstTurn() {
-        Game ticTacToeGame = new Game();
         Assert.assertEquals('X', ticTacToeGame.getPlayer(1));
+    }
+
+    @Test
+    public void shouldReturnOonSecondTurn(){
+        Assert.assertEquals('O',ticTacToeGame.getPlayer(2));
     }
 
 }
